f417cf61ce9268f1c161941a925ba75bdb5c93dc
Thomas Butman <thomas.butman@gmail.com>
2018-01-22 17:53:47 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+
+const app = require("express")();
+const fs = require("fs");
+const stripe = require("stripe")(keySecret);
+
+function logChargeData(charge){
+    fs.readFile('./data/charges.json', function (err, data) {
+        var json = JSON.parse(data)
+        json.push(charge)
+
+        fs.writeFile('./data/charges.json', JSON.stringify(json))
+    })
+}
+
+app.set("view engine", "pug");
+app.use(require("body-parser").urlencoded({extended: false}));
+
+app.get("/", function (req, res) {
+    return res.render("index.pug", { keyPublishable: keyPublishable });
+});
+
+app.get("/charges", function (req, res) {
+    var json = require('./data/charges.json');
+
+    return res.render("charges.pug", { charges: json });
+});
+
+app.get("/data/charges", function (req, res) {
+    var json = require('./data/charges.json');
+
+    return res.json(json);
+});
+
+app.post("/charge", function (req, res) {
+    var amount = 500;
+
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    }).then(function (customer) {
+        return stripe.charges.create({
+            amount: amount,
+            description: "Sample Charge",
+            currency: "usd",
+            customer: customer.id
+        });
+    }).then(function (charge) {
+        logChargeData(charge);
+
+        return res.render("confirmation.pug");
+    });
+});
+
+app.listen(5000);
